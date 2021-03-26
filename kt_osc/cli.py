17b53279827451b13a4b5a091b8cbf29ce6fa74d
Valmir Franca <vfranca3@gmail.com>
2021-03-26 07:05:55 -0300

    Adicionada opcao --version

--- kt_osc/cli.py
+++ kt_osc/cli.py
@@ -2,15 +2,24 @@
 Scripts do console
 """
 from kt_osc import kt_osc
+from kt_osc import __version__
 import click
 
 
 @click.command()
-@click.argument("preco-final")
-@click.argument("preco-inicial")
-def cli(preco_final, preco_inicial):
+@click.argument("preco-final", type=float, required=False)
+@click.argument("preco-inicial", type=float, required=False)
+@click.option("--version", is_flag=True, help="Exibe a versao")
+def cli(preco_final, preco_inicial, version):
     """Calcula oscilacao entre preco final e preco inicial."""
-    click.echo(kt_osc.osc(float(preco_final), float(preco_inicial)))
+    if version:
+        click.echo("kt-osc %s" % __version__)
+        return 0
+    if not preco_final:
+        click.echo("Usage: osc [OPTIONS] [PRECO-FINAL] [PRECO-INCIAL]")
+        return 0
+    click.echo(kt_osc.osc(preco_final, preco_inicial))
+    return 0
 
 
 if __name__ == "__main__":
